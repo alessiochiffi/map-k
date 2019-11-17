4b3617fa25182cbe57c277d88239c3067c513642
Alessioch <alessiochf@gmail.com>
2019-11-17 20:23:48 +0000

    added wwather component

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -6,8 +6,11 @@ import Place from '../places/places'
 import Loader from '../loader/loader';
 import Marker from './marker';
 import './map.css';
+import '../weather/weather.scss';
+
 
 const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY
+
 
 class Map extends Component {
   constructor(props) {
@@ -28,10 +31,10 @@ class Map extends Component {
   showCurrentLocation = () => {
     let options = {
         enableHighAccuracy: true,
-        timeout: 200,
-        maximumWait: 100,     // max wait time for desired accuracy
+        timeout: 100000,
+        maximumWait: 1000,     // max wait time for desired accuracy
         maximumAge: 0,          // disable cache
-        desiredAccuracy: 30,    // meters
+        desiredAccuracy: 300,    // meters
         fallbackToIP: true,     // fallback to IP if Geolocation fails or rejected
     };
 
@@ -47,10 +50,13 @@ class Map extends Component {
             initiated: true,
             isLoading: false,
           })
+          console.log(this.state);
+          this.showWeather()
           return;
         }
     });
   }
+
 
   componentDidMount() {
     this.showCurrentLocation();
@@ -62,9 +68,10 @@ class Map extends Component {
         lat: center.lat,
         lng: center.lng
       },
-      isLoading: false,
       hasResults: false
     })
+    this.showWeather()
+    console.log(this.state)
   }
 
   renderPlaces() {
@@ -72,6 +79,37 @@ class Map extends Component {
       return (
           <Place key={place.id} place={place} />
         );
+    })
+  }
+
+  renderWeather() {
+    return this.state.weather ?
+      (
+        <div className="weather__widget">
+          <div className="weather__location">
+            <span>{this.state.weather.name}</span>
+          </div>
+          <div className="weather__details">
+            <span>Temp <b>{this.state.weather.main.temp} C</b></span>
+            <div className="weather__values">
+              <span>{this.state.weather.weather[0].main}</span>
+              <img alt="icon" src={`http://openweathermap.org/img/w/${this.state.weather.weather[0].icon}.png`} />
+            </div>
+          </div>
+        </div>
+      )
+    : null;
+  }
+
+  showWeather() {
+    axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${this.state.currentLatLng.lat}&lon=${this.state.currentLatLng.lng}&units=metric&appid=3fb9669ae49b25530b94f992b51a6343`)
+    .then((response) => {
+      this.setState({
+        weather: response.data,
+      })
+    })
+    .catch((error) => {
+      console.log(error);
     })
   }
 
@@ -114,6 +152,7 @@ class Map extends Component {
             <div style={{ height: '50vh', width: '50wv' }}>
               <GoogleMapReact
                 bootstrapURLKeys={{ key: GOOGLE_API_KEY }}
+                defaultCenter={currentLatLng}
                 center={currentLatLng}
                 defaultZoom={14}
                 onChange={this._onChange}
@@ -126,7 +165,9 @@ class Map extends Component {
             <div className="searchPlaces">
               <button className="btn btn-primary" onClick={searchPlaces}>Search</button>
             </div>
-            <span>{currentLatLng.lat}-{currentLatLng.lng}</span>
+            <span className="map__coords">Coords {currentLatLng.lat}-{currentLatLng.lng}</span>
+              {this.renderWeather() ? this.renderWeather() : null}
+
           </div>
           <div className="results-panel container">
             {this.renderPlaces() ? this.renderPlaces() : null}
